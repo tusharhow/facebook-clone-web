f4afc3920f155036217c1ba6d2bffc0d3877eb56
tusharhow <tusharsoftdev@gmail.com>
2021-07-09 21:36:55 -0700

    Homepage UI Created

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final bgColor = Color(0xffF0F2F5);
+final loginBColor = Color(0xff166FE5);
+final signupBColor = Color(0xff42b72a);
+final buttonInputColor = Color(0xffECEDEE);
---
+++ lib/main.dart
@@ -0,0 +1,200 @@
+import 'dart:html';
+
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:facebook_clone/constants.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: 650,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: bgColor,
+        ),
+        child: Row(
+          children: [
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 200, 0, 0),
+                  child: SvgPicture.asset(
+                    'images/facebook.svg',
+                    color: loginBColor,
+                    height: 100,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(210, 0, 0, 0),
+                  child: Text(
+                    'Facebook helps you connect and share\n with the people in your life.',
+                    style: TextStyle(fontSize: 25),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              width: 300,
+            ),
+            Container(
+              height: 370,
+              width: 400,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(6),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    // offset: Offset(1, 1), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  Form(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          height: 15,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Email address or phone number',
+                              labelStyle: TextStyle(
+                                color: Colors.black26,
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(8.0),
+                                ),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black12),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(8.0),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          height: 15,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              labelStyle: TextStyle(
+                                color: Colors.black26,
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black12),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(8.0),
+                                ),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black12),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(8.0),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          height: 15,
+                        ),
+                        Container(
+                          child: Center(
+                            child: Text(
+                              'Log In',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16),
+                            ),
+                          ),
+                          height: 43,
+                          width: MediaQuery.of(context).size.width / 4,
+                          decoration: BoxDecoration(
+                            color: loginBColor,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          height: 20,
+                        ),
+                        Text(
+                          'Forgotten password?',
+                          style: TextStyle(color: Colors.blue, fontSize: 14),
+                        ),
+                        Divider(
+                          height: 50,
+                          endIndent: 20,
+                          indent: 20,
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          height: 4,
+                        ),
+                        Container(
+                          child: Center(
+                            child: Text(
+                              'Create New Account',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16),
+                            ),
+                          ),
+                          height: 43,
+                          width: MediaQuery.of(context).size.width / 9,
+                          decoration: BoxDecoration(
+                            color: signupBColor,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
